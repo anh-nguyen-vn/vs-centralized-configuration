4a9ef128d9b5860fd46aef0b419caebba00e0489
Ngoc Anh Nguyen <anhndn.1989@gmail.com>
2020-11-25 23:57:06 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Dev Project - Youtube videos sharing
+* Centralized-Configuration
+
+### Project Requirements
+* JDK 1.8.x
+* Maven 3.6.x
---
+++ src/main/java/com/anhndn/assessment/centralizedconfiguration/CentralizedConfigurationApplication.java
@@ -0,0 +1,14 @@
+package com.anhndn.assessment.centralizedconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CentralizedConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedConfigurationApplication.class, args);
+	}
+}
